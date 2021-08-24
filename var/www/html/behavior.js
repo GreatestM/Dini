cc5d320c2046bcc2a72c73669537c63862a6b8e6
root <root@ip-10-250-30-87.ec2.internal>
2021-08-24 12:25:14 +0000

    initial commit

---
+++ var/www/html/behavior.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.addEventListener("click", function() {
+	this.classList.toggle('small'); });
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.className ="";
+});
+
